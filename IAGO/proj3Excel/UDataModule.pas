cba8a3db8095c6598fbf61449c6ac1c07de69b21
IagoRampazzo <iagorampazzo@hotmail.com>
2014-04-15 11:24:36 -0300

    Proj excel
    asd

---
+++ IAGO/proj3Excel/UDataModule.pas
@@ -0,0 +1,28 @@
+unit UDataModule;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB;
+
+type
+  TDM = class(TDataModule)
+    ADOConnection1: TADOConnection;
+    ADOBD: TADOQuery;
+    ADOAux: TADOQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DM: TDM;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
